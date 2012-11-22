fff31b0997a2d3cba3b1a1ea56bc2bed7436697b
uni <uni@20c3f6aa-ec3b-0a4d-94da-d3b62f7bcf7d>
2012-11-22 19:04:18 +0000

    Добавлен пример проекта freemodbus на ATmega16.

---
+++ freemodbus/Version.h
@@ -0,0 +1,32 @@
+///////////////////////////////////////////////////////////
+//  Version.h
+//  Implementation of the Class CVersion
+//  Created on:      23 ноября 2012 г. 0:59:59
+//  Original author: Мезенцев В. Н.
+///////////////////////////////////////////////////////////
+
+#if !defined(EA_E62E5FD4_3D22_4581_B2F7_BE7DF95D3FBC__INCLUDED_)
+#define EA_E62E5FD4_3D22_4581_B2F7_BE7DF95D3FBC__INCLUDED_
+
+#include "Defines.h"
+
+/**
+ * Образ для работы с версией программы
+ */
+class CVersion {
+
+public:
+
+    inline static uint8_t GetMajor() { return 0; }
+    inline static uint8_t GetMinor() { return 0; }
+    inline static uint16_t GetRevision() { return 0; }
+    inline static uint16_t GetBuild() { return 24; }
+
+    inline static FCHAR_PTR GetBuildDateString() {
+        FLASHSTR_DECLARE( char, szBuildDateString, "23 ноября 2012 г." );
+        return szBuildDateString;
+    }
+
+};
+
+#endif // !defined(EA_E62E5FD4_3D22_4581_B2F7_BE7DF95D3FBC__INCLUDED_)
