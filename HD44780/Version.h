0c08ec95c1cd5659213affcb8892f125296fb88b
uni <uni@20c3f6aa-ec3b-0a4d-94da-d3b62f7bcf7d>
2012-12-03 04:25:24 +0000

    HD44780: Небольшие исправления проекта. Прошивка протестирована на отладочной плате. Проблема с битом готовности при моделировании в Proteus.

--- HD44780/Version.h
+++ HD44780/Version.h
@@ -1,7 +1,7 @@
 ///////////////////////////////////////////////////////////
 //  Version.h
 //  Implementation of the Class CVersion
-//  Created on:      2 декабря 2012 г. 18:07:12
+//  Created on:      3 декабря 2012 г. 10:21:22
 //  Original author: Мезенцев В. Н.
 ///////////////////////////////////////////////////////////
 
@@ -23,10 +23,10 @@ public:
     inline static uint8_t GetMajor() { return 0; }
     inline static uint8_t GetMinor() { return 0; }
     inline static uint16_t GetRevision() { return 0; }
-    inline static uint16_t GetBuild() { return 11; }
+    inline static uint16_t GetBuild() { return 40; }
 
     inline static FCHAR_PTR GetBuildDateString() {
-        return SPSTR( "2 декабря 2012 г." );
+        return SPSTR( "3 декабря 2012 г." );
     }
 
 };
