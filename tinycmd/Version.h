20537bf2a68736f4cbc96de4510af9b3d90d43ff
uni <uni@20c3f6aa-ec3b-0a4d-94da-d3b62f7bcf7d>
2013-10-09 10:00:46 +0000

    tinycmd: Исправлена проблема с VK_BACK. Изменено расположение полей в структуре буфера FIFO (fifo.h). Вероятно, причина была в этом.

--- tinycmd/Version.h
+++ tinycmd/Version.h
@@ -1,7 +1,7 @@
 ///////////////////////////////////////////////////////////
 //  Version.h
 //  Implementation of the Class CVersion
-//  Created on:      6 октября 2013 г. 22:45:50
+//  Created on:      9 октября 2013 г. 15:55:12
 //  Original author: Мезенцев В. Н.
 ///////////////////////////////////////////////////////////
 
@@ -21,15 +21,15 @@ class CVersion {
 public:
 
     inline static uint32_t GetMajor() { return 0; }
-    inline static uint32_t GetMinor() { return 0; }
-    inline static uint32_t GetBuild() { return 0; }
-    inline static uint32_t GetRevision() { return 32; }
+    inline static uint32_t GetMinor() { return 1; }
+    inline static uint32_t GetBuild() { return 5030; }
+    inline static uint32_t GetRevision() { return 28656; }
 
     inline static FCHAR_PTR GetVersionString() {
-        return SPSTR( "0.0.0.32" );
+        return SPSTR( "0.1.5030.28656" );
     }
     inline static FCHAR_PTR GetBuildDateString() {
-        return SPSTR( "6 октября 2013 г." );
+        return SPSTR( "9 октября 2013 г." );
     }
 
 };
