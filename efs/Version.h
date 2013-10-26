c8c46429fbf7725b0c6ed95df5fe546d03d35345
Viacheslav Mezentsev <viacheslavmezentsev@gmail.com>
2013-10-26 10:03:55 +0000

    efs: небольшие исправления в настройках сборки.

--- efs/Version.h
+++ efs/Version.h
@@ -1,7 +1,7 @@
 ///////////////////////////////////////////////////////////
 //  Version.h
 //  Implementation of the Class CVersion
-//  Created on:      26 октября 2013 г. 0:02:59
+//  Created on:      26 октября 2013 г. 16:01:33
 //  Original author: Мезенцев В. Н.
 ///////////////////////////////////////////////////////////
 
@@ -23,10 +23,10 @@ public:
     inline static uint32_t GetMajor() { return 0; }
     inline static uint32_t GetMinor() { return 1; }
     inline static uint32_t GetBuild() { return 5047; }
-    inline static uint32_t GetRevision() { return 89; }
+    inline static uint32_t GetRevision() { return 28846; }
 
     inline static FCHAR_PTR GetVersionString() {
-        return SPSTR( "0.1.5047.89" );
+        return SPSTR( "0.1.5047.28846" );
     }
     inline static FCHAR_PTR GetBuildDateString() {
         return SPSTR( "26 октября 2013 г." );
